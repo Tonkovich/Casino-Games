a4c2d3f7c76b0aa8f14d901ad7992c1d6d06be6c
Tonk <a_tonkovich@yahoo.com>
2018-01-07 16:12:26 -0600

    Added main method

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -3,4 +3,7 @@ public class Server {
  * The server class will be mainly responsible for interpreting incoming client messages and sending the data to the
  * appropriate method/location
  */
+    public static void main(String[] args) {
+
+    }
 }
