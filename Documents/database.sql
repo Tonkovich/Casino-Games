8d35826659e584a3a7edacea49c296516500df77
Tonk <a_tonkovich@yahoo.com>
2018-02-01 18:08:14 -0600

    Added statistics capabilities

--- Documents/database.sql
+++ Documents/database.sql
@@ -1,8 +1,20 @@
 # Used to backup players wallets, possibly add more later
 CREATE TABLE Players
 (
-  user_id INTEGER NOT NULL AUTO_INCREMENT,
-  wallet  DOUBLE NOT NULL,
+  user_id  INTEGER     NOT NULL AUTO_INCREMENT,
+  password VARCHAR(50) NOT NULL,
+  wallet   DOUBLE      NOT NULL,
   PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Statistics (
+  id          INTEGER NOT NULL AUTO_INCREMENT,
+  wins        INTEGER,
+  loses       INTEGER,
+  largest_win INTEGER,
+  user_id     INTEGER NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES Players (user_id)
+    ON DELETE CASCADE
 );
 
