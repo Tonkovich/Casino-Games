838cae01cc27911bf54970a7ebc70ef083e45b22
Tonk <a_tonkovich@yahoo.com>
2018-02-22 03:09:24 -0600

    Added values to Suit enum

--- src/Server/src/Models/Parts/CardGame/Suit.java
+++ src/Server/src/Models/Parts/CardGame/Suit.java
@@ -1,8 +1,37 @@
 package Models.Parts.CardGame;
 
 public enum Suit {
-    HEARTS,
-    SPADES,
-    CLUBS,
-    DIAMONDS
+    HEARTS("Hearts"),
+    SPADES("Spades"),
+    CLUBS("Clubs"),
+    DIAMONDS("Diamonds");
+
+    private String suitValue;
+
+    Suit(String value) {
+        this.suitValue = value;
+    }
+
+    public String getVal() {
+        return suitValue;
+    }
+
+    public int getInt() {
+        int num = 0;
+        switch (suitValue) {
+            case "Hearts":
+                num = 1;
+                break;
+            case "Spades":
+                num = 2;
+                break;
+            case "Clubs":
+                num = 3;
+                break;
+            case "Diamonds":
+                num = 4;
+                break;
+        }
+        return num;
+    }
 }
