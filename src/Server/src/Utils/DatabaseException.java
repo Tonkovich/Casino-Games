80268893b8d2f458763d46eee781a1611254b708
Tonk <a_tonkovich@yahoo.com>
2018-02-25 03:30:32 -0600

    Added DatabaseException for easy troubleshooting

---
+++ src/Server/src/Utils/DatabaseException.java
@@ -0,0 +1,12 @@
+package Utils;
+
+public class DatabaseException extends Exception {
+
+    public DatabaseException() {
+
+    }
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
