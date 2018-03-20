aa2f9d6b1dcb446539487389a908ea7e8078242c
Tonk <a_tonkovich@yahoo.com>
2018-03-19 19:56:01 -0500

    Test update: cardRankTest() now loops test 20 times cause of randomness

--- src/Server/tests/PokerTest.java
+++ src/Server/tests/PokerTest.java
@@ -9,29 +9,32 @@ public class PokerTest {
     public void cardRankTest() {
         Players playersDB = Players.getInstance(); // Simulate player login
 
-        Player user1 = new Player();
-        user1.setUserID(1);
-        user1.setUsername("Player 1");
-        Player user2 = new Player();
-        user2.setUserID(2);
-        user2.setUsername("Player 2");
-        Player user3 = new Player();
-        user3.setUserID(3);
-        user3.setUsername("Player 3");
-        playersDB.loginPlayer(1, user1);
-        playersDB.loginPlayer(2, user2);
-        playersDB.loginPlayer(3, user3); // All players logged in
+        for (int i = 0; i < 20; i++) {
+            Player user1 = new Player();
+            user1.setUserID(1);
+            user1.setUsername("Player 1");
+            Player user2 = new Player();
+            user2.setUserID(2);
+            user2.setUsername("Player 2");
+            Player user3 = new Player();
+            user3.setUserID(3);
+            user3.setUsername("Player 3");
+            playersDB.loginPlayer(1, user1);
+            playersDB.loginPlayer(2, user2);
+            playersDB.loginPlayer(3, user3); // All players logged in
 
-        Poker poker = new Poker(); // Game instance started
+            Poker poker = new Poker(); // Game instance started
 
-        poker.addPlayer(1, user1);
-        poker.addPlayer(2, user2);
-        poker.addPlayer(3, user3); // All players have joined
+            poker.addPlayer(1, user1);
+            poker.addPlayer(2, user2);
+            poker.addPlayer(3, user3); // All players have joined
 
-        poker.deal(); // Start game
+            poker.deal(); // Start game
 
-        poker.drawNextCard();
-        poker.drawNextCard(); // River done
-        poker.getWinner();
+            poker.drawNextCard();
+            poker.drawNextCard(); // River done
+            poker.getWinner();
+            System.out.print("\n\n"); // Space out each test
+        }
     }
 }
