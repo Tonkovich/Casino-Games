aa678f08ba41385202013e2851c4e829895f0052
Tonk <a_tonkovich@yahoo.com>
2018-04-05 02:07:56 -0500

    Database connection is now called by getInstance()

--- src/Server/src/Parsers/AccessParse.java
+++ src/Server/src/Parsers/AccessParse.java
@@ -3,12 +3,11 @@ package Parsers;
 import Utils.Database;
 
 import javax.json.JsonObject;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class AccessParse {
     private static AccessParse instance;
-    private static Database db;
+    private static Database db = Database.getInstance();
+
 
     private AccessParse() {
     }
@@ -16,19 +15,13 @@ public class AccessParse {
     public static AccessParse getInstance() {
         if (instance == null) {
             instance = new AccessParse();
-            db = Database.getInstance();
         }
         return instance;
     }
 
-    public void parse(JsonObject obj) {
+    public void parse(JsonObject obj, String ip) {
         String username = obj.getString("username");
         String password = obj.getString("password");
-        try {
-            InetAddress ip = InetAddress.getByName(obj.getString("IPAddress"));
-            db.loginPlayer(username, password, ip);
-        } catch (UnknownHostException ex) {
-            // TODO: Stop parse
-        }
+        db.loginPlayer(username, password, ip);
     }
 }
