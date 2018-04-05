4a610112ce48f088082431b969b119d40d5dfa09
Tonk <a_tonkovich@yahoo.com>
2018-04-05 02:04:39 -0500

    Database connection is now established in startConnection() method and Database class is now singleton Bug fix: Player now logs in correctly

--- src/Server/src/Utils/Database.java
+++ src/Server/src/Utils/Database.java
@@ -5,7 +5,6 @@ import Utils.JSONMessages.LoginMessages;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
-import java.net.InetAddress;
 import java.util.List;
 
 public class Database {
@@ -16,6 +15,9 @@ public class Database {
     private static Database instance;
 
     public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
         return instance;
     }
 
@@ -24,8 +26,7 @@ public class Database {
     }
 
     // Calling this constructor creates sql2o object
-    public Database(String jdbcURL, String jdbcUser, String jdbcPass) throws DatabaseException {
-        instance = this;
+    public void startConnection(String jdbcURL, String jdbcUser, String jdbcPass) throws DatabaseException {
         if (sql2o == null) { // Only do this once
             if (jdbcURL == null || jdbcURL.length() == 0 || jdbcUser == null || jdbcPass == null) {
                 throw new DatabaseException("Database not connecting"
@@ -33,33 +34,44 @@ public class Database {
                         + "\n User: " + jdbcUser
                         + "\n Pass: " + jdbcPass);
             }
-
             sql2o = new Sql2o(jdbcURL, jdbcUser, jdbcPass);
         }
     }
 
     // Will login player and load player from DB
-    public void loginPlayer(String username, String password, InetAddress ip) {
-        String playerSQL = "SELECT user_id, wallet FROM Players p WHERE p.username = :username AND p.password = :password";
+    public void loginPlayer(String username, String password, String ip) {
+        System.out.println(username + " " + ip);
+        updateIP(ip, username);
+        String playerSQL = "SELECT user_id, ip, username, wallet FROM Players p WHERE p.username = :username AND p.password = :password";
         try (Connection con = sql2o.open()) {
             List<Player> player = con.createQuery(playerSQL)
                     .addParameter("username", username)
                     .addParameter("password", password)
-                    .addColumnMapping("username", username)
+                    .addColumnMapping("username", "username")
                     .addColumnMapping("wallet", "wallet")
                     .addColumnMapping("user_id", "userID")
-                    .addColumnMapping("ip_address", ip.getHostAddress())
+                    .addColumnMapping("ip", "ip")
                     .executeAndFetch(Player.class);
 
             // Push player to Server Map and send success message
-            Player newPlayer = player.get(0);
-            if (newPlayer.getUserID() != 0) { // Should work to verified login worked
+            Player newPlayer = new Player();
+            if (player.size() == 1) {
+                newPlayer = player.get(0);
                 players.loginPlayer(newPlayer.getUserID(), newPlayer);
                 newPlayer.sendMessage(lm.successLogin());
             } else {
-                // Should work since IP will still be entered but other fields should be null
+                newPlayer.setIPAddress(ip);
                 newPlayer.sendMessage(lm.incorrectLogin());
             }
         }
+    }
+
+    private void updateIP(String ip, String username) {
+        String sql = "UPDATE Players p SET p.ip = :ip WHERE p.username = :username";
+        Connection con = sql2o.open();
+        con.createQuery(sql)
+                .addParameter("username", username)
+                .addParameter("ip", ip)
+                .executeUpdate();
     }
 }
