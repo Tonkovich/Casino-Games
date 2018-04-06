935fce15368e5998132e26ae9ab55275a3d18a85
Tonk <a_tonkovich@yahoo.com>
2018-04-05 21:44:44 -0500

    Moved parsing of login status to Login instead of LoginMessages

--- src/Client/src/Utils/JSONMesssages/LoginMessages.java
+++ src/Client/src/Utils/JSONMesssages/LoginMessages.java
@@ -1,16 +1,11 @@
 package Utils.JSONMesssages;
 
-import Utils.ClientSocket;
-
 import javax.json.Json;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
-import java.io.IOException;
-import java.io.StringReader;
 
 public class LoginMessages {
 
-    public boolean login(String username, String password, ClientSocket mySocket) {
+    public String login(String username, String password) {
         JsonObject json = Json.createObjectBuilder()
                 .add("login", Json.createObjectBuilder()
                         .add("username", username)
@@ -18,31 +13,7 @@ public class LoginMessages {
                         .build()
                 ).build();
 
-        boolean response = false;
-
-        try {
-            mySocket.sendMessage(json.toString());
-            JsonReader jsonReader = Json.createReader(new StringReader(mySocket.receiveMessage()));
-            // TODO Timer to disconnect, assume server crashed
-            JsonObject obj = jsonReader.readObject();
-
-            String login = obj.getString("login");
-            switch (login) {
-                case "reject":
-                    break;
-                case "success":
-                    response = true;
-                    break;
-                default:
-                    break;
-            }
-
-
-        } catch (IOException ex) {
-            ex.getMessage();
-        }
-
-        return response;
+        return json.toString();
     }
 
 }
