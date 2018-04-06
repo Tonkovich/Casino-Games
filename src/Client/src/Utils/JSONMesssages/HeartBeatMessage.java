bb9aa5e303e4e34addc8b55db0d16f6c0d43026b
Tonk <a_tonkovich@yahoo.com>
2018-04-05 21:46:02 -0500

    Added return heart beat message to client

---
+++ src/Client/src/Utils/JSONMesssages/HeartBeatMessage.java
@@ -0,0 +1,14 @@
+package Utils.JSONMesssages;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class HeartBeatMessage {
+    public String heartBeatSend(int userID) {
+        JsonObject json = Json.createObjectBuilder()
+                .add("header", "serverMessage")
+                .add("heartBeat", "Yes")
+                .add("userID", userID).build();
+        return json.toString();
+    }
+}
