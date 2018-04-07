cd02ab36f72c161038e1d2da92f3c0e96807cd1e
Tonk <a_tonkovich@yahoo.com>
2018-04-06 19:14:48 -0500

    Solidifying login and heartbeat message standard

--- src/Server/src/Utils/JSONMessages/LoginMessages.java
+++ src/Server/src/Utils/JSONMessages/LoginMessages.java
@@ -7,7 +7,6 @@ public class LoginMessages {
 
     public String incorrectLogin() {
         JsonObject json = Json.createObjectBuilder()
-                .add("header", "serverMessage")
                 .add("login", "reject").build();
         return json.toString();
     }
@@ -16,7 +15,6 @@ public class LoginMessages {
         // Send data on login
 
         JsonObject json = Json.createObjectBuilder()
-                .add("header", "serverMessage")
                 .add("login", "success")
                 .add("userID", userID)
                 .add("wallet", wallet).build();
