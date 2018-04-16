cbcd90dc76ab0eff92412051774db1247dbd49eb
Tonk <a_tonkovich@yahoo.com>
2018-04-15 21:37:33 -0500

    Added: upon join/create client is notified to open GUI, and added additional logging

--- src/Server/src/Utils/Database/Players.java
+++ src/Server/src/Utils/Database/Players.java
@@ -2,6 +2,8 @@ package Utils.Database;
 
 import Models.Games.Player;
 import Utils.Packet.HeartBeat;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -13,6 +15,7 @@ public class Players {
     private HashMap<Integer, Player> players = new HashMap<>();
     private int playerCount = players.size();
     private static HeartBeat hb;
+    private static final Logger log = LogManager.getLogger(Players.class);
 
     public static Players getInstance() {
         if (instance == null) {
@@ -33,9 +36,11 @@ public class Players {
     public void loginPlayer(int userID, Player player) {
         players.put(userID, player);
         hb.start(); // Wont do anything if already running. See start()
+        log.info(player.getUsername() + " logged in.");
     }
 
     public void logoutPlayer(int userID) {
+//        log.info(getPlayer(userID).getUsername() + " logged out.");
         players.remove(userID);
         if (players.size() == 0) {
             hb.stop(); // Stop heartbeat
